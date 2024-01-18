61a160958f174e95b4aa4f38d3fd7c5d9c6efd8a
Matthew <78713317+mreed4@users.noreply.github.com>
2024-01-17 19:28:10 -0800

    Flesh out

--- app.js
+++ app.js
@@ -1,7 +1,60 @@
-const incomeInput = document.getElementById("income");
-const addExpenseDiv = document.getElementById("add-expense");
-const [expenseNameInput, expenseAmountInput] = addExpenseDiv.querySelectorAll("input");
-const summaryPanel = document.getElementById("summary-panel");
-const [totalIncome, totalExpense, balance] = summaryPanel.querySelectorAll(".summary-amount");
-const expenseList = document.getElementById("expense-table");
-let expenseItems = [];
+import { expenses } from "./data.js";
+
+(function init() {
+  renderExpenses(expenses);
+  initEventListeners();
+  updateSummary();
+})();
+
+function initEventListeners() {
+  const incomeInput = document.querySelector("input#income");
+
+  incomeInput.addEventListener("input", updateSummary);
+}
+
+function renderExpenses(expenses) {
+  const expenseTable = document.querySelector(".expense-table");
+
+  expenses.forEach((expense) => {
+    const expenseItem = buildExpenseItem(expense);
+    expenseTable.insertAdjacentHTML("beforeend", expenseItem);
+  });
+
+  return expenses;
+}
+
+function buildExpenseItem(expense) {
+  const { name, amount } = expense;
+
+  return `
+      <div class="expense-name">${name}</div>
+      <div class="expense-amount">${amount}</div>
+      <div class="delete">
+        <button name="delete-expense" class="delete-expense">
+          <img src="./images/trash.svg" alt="Delete expense" />
+        </button>
+      </div>`;
+}
+
+function updateSummary() {
+  const summaryPanel = document.querySelector(".summary-panel");
+  const [incomeSummary, expenseSummary, balanceSummary] = summaryPanel.querySelectorAll(".summary-amount");
+  const income = Number(document.getElementById("income").value);
+  const expenseItems = [...document.querySelectorAll(".expense-amount")].map((item) => Number(item.textContent));
+  const expense = expenseItems.reduce((acc, item) => acc + item, 0);
+  const balance = income - expense;
+
+  incomeSummary.textContent = formatCurrency(income);
+  expenseSummary.textContent = formatCurrency(expense);
+  balanceSummary.textContent = formatCurrency(balance);
+
+  if (balance < 0) {
+    balanceSummary.classList.add("negative");
+  } else {
+    balanceSummary.classList.remove("negative");
+  }
+}
+
+function formatCurrency(amount) {
+  return amount.toLocaleString("en-US", { style: "currency", currency: "USD" });
+}
---
+++ data.js
@@ -0,0 +1,18 @@
+export const expenses = [
+  {
+    name: "Rent",
+    amount: 1000,
+  },
+  {
+    name: "Groceries",
+    amount: 200,
+  },
+  {
+    name: "Gas",
+    amount: 100,
+  },
+  {
+    name: "Car Payment",
+    amount: 300,
+  },
+];
